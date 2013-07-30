a03418aac3e7c591b97dd4bf04166da82da475b5
tatsuya kobayashi <kobainfo@gmail.com>
2013-07-30 15:28:56 +0900

    Some file path improvements

--- install.sh
+++ install.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 cd $(dirname $0)
-for dotfile in .?*
+ 
+DOT_FILES=( .screenrc .zshrc .zshrc.local)
+
+for file in ${DOT_FILES[@]}
 do
-    if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
-    then
-        ln -Fis "$PWD/$dotfile" $HOME
-    fi
+  ln -Fis "$PWD/$file" $HOME
 done
+
+# jenkins
+ln -Fis "$PWD/.jenkins/myjenkins.css" $HOME/.jenkins/userContent
+ln -Fis "$PWD/.jenkins/myjenkins.js" $HOME/.jenkins/userContent
+
+# ssh
+ln -Fis "$PWD/.ssh/config" $HOME/.jenkins/userContent
+
+
+# for dotfile in .?*
+# do
+#     if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
+#     then
+#         ln -Fis "$PWD/$dotfile" $HOME
+#     fi
+# done
 
