9af60f7bef5122c6623a7895501f89433483645d
tatsuya kobayashi <kobainfo@gmail.com>
2013-07-30 14:05:39 +0900

    add jenkins files

---
+++ .jenkins/myjenkins.js
@@ -0,0 +1,6 @@
+// Jenkins js tweaks
+setTimeout(function(){  
+  document.getElementById('search-box').setAttribute("type","search");
+  document.getElementById('main-table').setAttribute("style","none");
+}, 350);
+
